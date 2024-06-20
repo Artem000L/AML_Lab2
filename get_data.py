c00130ba99fbd9bea3445cdaabf14afb402e44d3
Artem000L <mctema007@bk.ru>
2024-06-20 09:54:42 +0700

    change commit

--- get_data.py
+++ get_data.py
@@ -1,8 +1,12 @@
 import requests
+import os
+
 
 # Получение данных
 data = 'https://archive.ics.uci.edu/static/public/186/data.csv'
 response = requests.get(data)
+
+os.makedirs('data', exist_ok=True)
 
 # Сохранение в файл
 with open('data/wine.csv', 'wb') as file:
