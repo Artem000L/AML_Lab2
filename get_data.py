8f7dc08f010249f786234b4e3c69b2d794bfb3f8
Artem000L <mctema007@bk.ru>
2024-06-20 10:44:57 +0700

    change get_data.py

--- get_data.py
+++ get_data.py
@@ -4,7 +4,7 @@ import os
 
 # Получение данных
 data = 'https://archive.ics.uci.edu/static/public/186/data.csv'
-response = requests.get(data)
+response = requests.get(data, verity=False)
 
 os.makedirs('data', exist_ok=True)
 
